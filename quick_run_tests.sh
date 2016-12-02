ce1c84e75e92dcfadd7c42738bdb99c42bce730d
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-02 02:01:33 +0000

    Made the exit status of the tests be the exit status of the test scripts

--- quick_run_tests.sh
+++ quick_run_tests.sh
@@ -8,6 +8,9 @@ sleep 1
 
 # Run the tests
 docker run -t --rm --network cyberdojoselenium_default -e "browser=chrome" -v `pwd`/tests:/tests cyberdojo/selenium-test-environment
+RESULT=$?
 
 # Take down the selenium hub and nodes
 docker-compose --file=cyber_dojo_selenium/docker-compose.yml down
+
+exit $RESULT
--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,9 @@ sleep 1
 
 # Run the tests
 docker run -t --rm --network cyberdojoselenium_default cyberdojo/system-tests
+RESULT=$?
 
 # Take down the selenium hub and nodes
 docker-compose --file=cyber_dojo_selenium/docker-compose.yml down
+
+exit $RESULT
