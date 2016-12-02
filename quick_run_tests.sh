f74ff2c19812e5c028ba6fb7bffc8d82b67cbf62
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-02 01:37:44 +0000

    Created custom docker network to stop the name being based on the directory name

--- quick_run_tests.sh
+++ quick_run_tests.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Create a custom docker network and bring up selenium hub and nodes
+docker network create cyber_dojo_system_tests
 docker-compose up -d
-# TODO: Figure out where the network name 'cyberdojosystemtests_default' comes from
-# I assume it's the directory name but this will need to be fixed
-docker run -t --rm --network cyberdojosystemtests_default -e "browser=chrome" -v `pwd`/tests:/tests cyberdojo/selenium-test-environment
+
+# Wait for nodes to connect to the hub
+sleep 1
+
+# Run the tests
+docker run -t --rm --network cyber_dojo_system_tests -e "browser=chrome" -v `pwd`/tests:/tests cyberdojo/selenium-test-environment
+
+# Take down the selenium hub, nodes and the docker network
 docker-compose down
+docker network rm cyber_dojo_system_tests
