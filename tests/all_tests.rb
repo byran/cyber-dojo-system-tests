3faa8a6cb26ebc85add02d3c287c884720f323ff
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-04 12:11:58 +0000

    Added scanning for test files from all_tests.rb

--- tests/all_tests.rb
+++ tests/all_tests.rb
@@ -1,5 +1,12 @@
 def run_tests
-  require("./test_demo.rb")
+  directory = File.expand_path(File.dirname(__FILE__))
+  test_filter = File.join(directory, "**", "test_*.rb")
+
+  test_files = Dir.glob(test_filter)
+
+  test_files.each do |file|
+    require file
+  end
 end
 
 if ENV['browser'] == 'chrome'
