78f3e7376f90fd72dc00bc65f461a38a762c9573
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-04 22:38:00 +0000

    Added running tests against Firefox to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,10 +7,26 @@ docker-compose --file=cyber_dojo_selenium/docker-compose.yml up -d
 sleep 5
 
 # Run the tests
-docker run -t --rm --network cyberdojoselenium_default cyberdojo/system-tests
-RESULT=$?
+docker run -e "browser=chrome" -t --rm --network cyberdojoselenium_default cyberdojo/system-tests
+CHROME_RESULT=$?
+
+docker run -e "browser=firefox" -t --rm --network cyberdojoselenium_default cyberdojo/system-tests
+FIREFOX_RESULT=$?
 
 # Take down the selenium hub and nodes
 docker-compose --file=cyber_dojo_selenium/docker-compose.yml down
 
-exit $RESULT
+if [[ "$CHROME_RESULT" -eq "0" && "$FIREFOX_RESULT" -eq "0" ]]; then
+    exit 0
+fi
+
+if [ "$CHROME_RESULT" -ne "0" ]; then
+    exit $CHROME_RESULT
+fi
+
+if [ "$FIREFOX_RESULT" -ne "0" ]; then
+    exit $FIREFOX_RESULT
+fi
+
+# Should never get here but just in case
+exit 1
