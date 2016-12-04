4eee7c60572646ff6b774d2c7d3c1c429237ad8f
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-04 20:47:37 +0000

    Increased sleep to allow selenium nodes to connect to the hub as it's not long enough on travis

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@
 docker-compose --file=cyber_dojo_selenium/docker-compose.yml up -d
 
 # Wait for nodes to connect to the hub
-sleep 1
+sleep 5
 
 # Run the tests
 docker run -t --rm --network cyberdojoselenium_default cyberdojo/system-tests
