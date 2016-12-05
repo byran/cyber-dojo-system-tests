0601af18811ae1412e8b287a5ea96dac722b4319
Jon Jagger <jon@jaggersoft.com>
2016-12-05 07:55:11 +0000

    refactor to simpler require_relative

--- tests/browser/browser.rb
+++ tests/browser/browser.rb
@@ -1,7 +1,7 @@
 require 'selenium-webdriver'
 
-require File.join(File.expand_path(File.dirname(__FILE__)), "pages.rb")
-require File.join(File.expand_path(File.dirname(__FILE__)), "wait_mixin.rb")
+require_relative './pages.rb'
+require_relative './wait_mixin.rb'
 
 module CyberDojo
   class Browser
