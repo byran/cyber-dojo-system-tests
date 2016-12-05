afa7081723684d3cedf933f72f8b51a4070ef741
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-05 20:43:32 +0000

    Refactored to use single quotes instead of double quote for strings that are not interpreted and used require_relative

--- tests/test_Running_tests_in_a_kata.rb
+++ tests/test_Running_tests_in_a_kata.rb
@@ -1,4 +1,4 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), "cyber_dojo_test.rb")
+require_relative './cyber_dojo_test.rb'
 
 class RunningTestsInAKataTest < CyberDojoTest
 
@@ -10,9 +10,9 @@ class RunningTestsInAKataTest < CyberDojoTest
     browser.page.wait_for_spinner_to_show_and_hide
 
     assert_equal 1, browser.page.number_of_traffic_lights
-    assert_equal "red", browser.page.traffic_light(0).attribute("data-colour")
-    assert_equal browser.base_URL + "images/bulb_red.png", browser.page.traffic_light_image(0).attribute("src")
-    assert_equal "1", browser.page.traffic_light_count_element.text
+    assert_equal 'red', browser.page.traffic_light(0).attribute('data-colour')
+    assert_equal browser.base_URL + 'images/bulb_red.png', browser.page.traffic_light_image(0).attribute('src')
+    assert_equal '1', browser.page.traffic_light_count_element.text
   end
 
 end
