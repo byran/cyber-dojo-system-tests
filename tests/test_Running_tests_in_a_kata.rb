217296f539b42238a9cc8a5ccde8c2d1f0a45b5d
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-06 23:20:10 +0000

    Added test checking if invalid code adds a amber traffic light

--- tests/test_Running_tests_in_a_kata.rb
+++ tests/test_Running_tests_in_a_kata.rb
@@ -34,4 +34,22 @@ class RunningTestsInAKataTest < CyberDojoTest
     assert_equal '1', browser.page.traffic_light_count_element.text
   end
 
+  def test_A_syntax_error_in_the_code_adds_a_amber_traffic_light
+    create_kata
+
+    browser.page.select_file 'hiker.c'
+
+    browser.page.editor.clear
+    browser.page.editor.send_keys "abc\n"
+
+    browser.page.test_button.click
+
+    browser.page.wait_for_spinner_to_show_and_hide
+
+    assert_equal 1, browser.page.number_of_traffic_lights
+    assert_equal 'amber', browser.page.traffic_light(0).attribute('data-colour')
+    assert_equal browser.base_URL + 'images/bulb_amber.png', browser.page.traffic_light_image(0).attribute('src')
+    assert_equal '1', browser.page.traffic_light_count_element.text
+  end
+
 end
