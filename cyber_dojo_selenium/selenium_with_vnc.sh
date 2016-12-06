cdfe8327e1478284e8861e2e1158c7ab9f9a3924
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-06 13:52:35 +0000

    Added option to run the selenium nodes with a VNC viewer for debugging

---
+++ cyber_dojo_selenium/selenium_with_vnc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PORT=5901
+
+if [ "$1" == "firefox" ]; then
+    PORT=5900
+fi
+
+sed 's/node-firefox/node-firefox-debug/; s/node-chrome/node-chrome-debug/' <docker-compose.yml >docker-compose-debug.yml
+
+docker-compose --file=docker-compose-debug.yml up -d
+sleep 3
+vnc 127.0.0.1:$PORT
+docker-compose --file=docker-compose-debug.yml down
+
+rm -f docker-compose-debug.yml
