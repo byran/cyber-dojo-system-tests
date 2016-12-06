355942c7772ac26f72e7918c0d847f3edc676dbb
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-06 23:47:35 +0000

    Added test for a traffic light being added for each test run

--- tests/test_Running_tests_in_a_kata.rb
+++ tests/test_Running_tests_in_a_kata.rb
@@ -46,6 +46,33 @@ class RunningTestsInAKataTest < CyberDojoTest
     assert_traffic_light_at_index_is_colour 0, 'amber'
   end
 
+  def test_A_traffic_light_is_added_for_each_test_run
+    create_kata
+
+    browser.page.test_button.click
+    browser.page.wait_for_spinner_to_show_and_hide
+
+    browser.page.select_file 'hiker.c'
+    browser.page.editor.clear
+    browser.page.editor.send_keys "#include \"hiker.h\"\n"
+    browser.page.editor.send_keys "int answer() { return 42; }\n"
+
+    browser.page.test_button.click
+    browser.page.wait_for_spinner_to_show_and_hide
+
+    browser.page.select_file 'hiker.c'
+    browser.page.editor.clear
+    browser.page.editor.send_keys "abc\n"
+
+    browser.page.test_button.click
+    browser.page.wait_for_spinner_to_show_and_hide
+
+    assert_number_of_traffic_lights_is 3
+    assert_traffic_light_at_index_is_colour 0, 'red'
+    assert_traffic_light_at_index_is_colour 1, 'green'
+    assert_traffic_light_at_index_is_colour 2, 'amber'
+  end
+
   def assert_number_of_traffic_lights_is(count)
     assert_equal count, browser.page.number_of_traffic_lights
     assert_equal "#{count}", browser.page.traffic_light_count_element.text
