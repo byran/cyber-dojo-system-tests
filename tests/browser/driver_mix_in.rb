29c7ff4d31f0d3627a74f4c3f7c4bf81073f73da
Byran Wills-Heath <byran@adgico.co.uk>
2016-12-11 19:25:38 +0000

    Added driver mix in method to help finding elements during test development

--- tests/browser/driver_mix_in.rb
+++ tests/browser/driver_mix_in.rb
@@ -8,6 +8,32 @@ module CyberDojo
       execute_script('arguments[0].scrollIntoView(true);', element)
     end
 
+    def print_sub_elements(element)
+      print "\n============================\n"
+      print_sub_elements_with_indent element
+    end
+
+    def print_sub_elements_with_indent(element, indent = '')
+      print_element_details element, indent
+
+      sub_elements = element.find_elements :xpath => './*'
+      for sub_element in sub_elements
+        print_sub_elements_with_indent sub_element, indent + '| '
+      end
+    end
+
+    def print_element_details(element, indent = '')
+      print indent
+      print element.tag_name
+
+      id = element.attribute('id')
+      print ' - id: ' + id if !id.nil? && id != ''
+
+      element_class = element.attribute('class')
+      print ' - class: ' + element_class if !element_class.nil? && element_class != ''
+
+      print "\n"
+    end
   end
 
 end
