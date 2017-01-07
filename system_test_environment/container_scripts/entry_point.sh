06d126cd64b1adb1190ab7476f540a0b15a6df66
Byran Wills-Heath <byran@adgico.co.uk>
2017-01-07 15:27:42 +0000

    Fixed returning the incorrect exit status from a test run

--- system_test_environment/container_scripts/entry_point.sh
+++ system_test_environment/container_scripts/entry_point.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /tests
-ruby all_tests.rb 2>&1 | tee artifacts/log-`date +%Y-%m-%d-%H-%M-%S`.txt
+ruby all_tests.rb 2>&1 | tee artifacts/log-`date +%Y-%m-%d-%H-%M-%S`.txt ; test ${PIPESTATUS[0]} -eq 0
