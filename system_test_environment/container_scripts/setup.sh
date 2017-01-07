8a91eba6a3e987f599754581104eab631eb9bb1e
Byran Wills-Heath <byran@adgico.co.uk>
2017-01-07 16:28:10 +0000

    cyberdojo/system-test-environment image now base on cyberdojo/ruby

--- system_test_environment/container_scripts/setup.sh
+++ system_test_environment/container_scripts/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-apt-get update -qqy
+# Install packages need to build the gems
+apk --update --no-cache add build-base libffi-dev
 
-apt-get -qqy --no-install-recommends install \
-    build-essential \
-    ruby \
-    ruby-dev
+gem install selenium-webdriver -N
+gem install minitest -N
+gem install json -N
 
-rm -rf /var/lib/apt/lists/* /var/cache/apt/*
-
-gem install selenium-webdriver
+# Remove packages to keep the image small
+apk del build-base libffi-dev
