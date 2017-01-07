85b1a9232b2cd5bf3dd56cc18dceda6b16e594e2
Byran Wills-Heath <byran@adgico.co.uk>
2017-01-07 15:44:37 +0000

    Fix for possible stale element crash when on show exercises page

--- tests/browser/setup_default_start_point_show_exercises_page.rb
+++ tests/browser/setup_default_start_point_show_exercises_page.rb
@@ -53,11 +53,11 @@ module CyberDojo
     end
 
     def ok_button
-      buttons = @wait.until_with_message("Unable to find ok button in start coding dialog on \"setup_default_start_point/show_exercises\" page") {
-        @driver.find_elements(:class => 'ui-button-text')
+      @wait.until_with_message("Unable to find ok button in start coding dialog on \"setup_default_start_point/show_exercises\" page") {
+        buttons = @driver.find_elements(:class => 'ui-button-text')
+        index = buttons.find_index { |b| b.text == 'ok' }
+        buttons[index]
       }
-      index = buttons.find_index { |b| b.text == 'ok' }
-      buttons[index]
     end
 
   end
