2ae19902bcf20e0397bfb130dbc0b4d4f8504239
Jon Jagger <jon@jaggersoft.com>
2017-10-23 11:24:29 +0100

    setup button is now called setup_default_button to distinguish it from new setup_custom_button

--- tests/browser/main_page.rb
+++ tests/browser/main_page.rb
@@ -14,26 +14,26 @@ module CyberDojo
       end
     end
 
-    def setup_button
-      @wait.until_with_message('Unable to find setup button on home page') {
-        @driver.find_element(:id => 'setup-a-new-practice-session-button')
+    def setup_default_button
+      @wait.until_with_message('Unable to find [setup default] button on home page') {
+        @driver.find_element(:id => 'setup-a-new-default-practice-session-button')
       }
     end
 
     def enter_button
-      @wait.until_with_message('Unable to find enter button on home page') {
+      @wait.until_with_message('Unable to find [enter] button on home page') {
         @driver.find_element(:id => 'enter-button')
       }
     end
 
     def review_button
-      @wait.until_with_message('Unable to find review button on home page') {
+      @wait.until_with_message('Unable to find [review] button on home page') {
         @driver.find_element(:id => 'review-button')
       }
     end
 
     def donate_button
-      @wait.until_with_message('Unable to find donate button on home page') {
+      @wait.until_with_message('Unable to find [donate] button on home page') {
         @driver.find_element(:id => 'donate-button')
       }
     end
--- tests/browser/main_page_steps.rb
+++ tests/browser/main_page_steps.rb
@@ -5,8 +5,7 @@ module CyberDojo
   class MainPageSteps < PageSteps
 
     def start_setting_up_a_kata
-      setup_button.click
-
+      setup_default_button.click
       assert_page_loaded(pages.setup_default_start_point_show_languages)
     end
 
