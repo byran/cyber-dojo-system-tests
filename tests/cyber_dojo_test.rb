c744e96914d204ab856e4c8d7c4d8de6f289d9fa
Byran Wills-Heath <byran@adgico.co.uk>
2018-03-02 02:01:15 +0000

    Updated create_and_enter_kata for new UI

--- tests/cyber_dojo_test.rb
+++ tests/cyber_dojo_test.rb
@@ -60,12 +60,24 @@ class CyberDojoTest < Minitest::Test
     assert_page_loaded(pages.main)
   end
 
-  #def create_and_enter_kata(language = "C (gcc)", framework = "assert", exercise = "(Verbal)")
-  #  navigate_home
-  #  start_setting_up_a_kata
-  #  select_a_language_and_framework(language, framework)
-  #  select_an_exercise_then_enter_kata(exercise)
-  #end
+  def create_and_enter_kata(language = "C (gcc)", framework = "assert", exercise = "(Verbal)")
+    navigate_home
+
+    im_on_my_own_button.click
+    assert_page_loaded(pages.individual)
+
+    create_a_new_session_button.click
+    assert_page_loaded(pages.setup_default_start_point_show)
+
+    select_display_name('C (gcc), assert')
+    select_exercise('(Verbal)')
+    ok_button.click
+    assert_page_loaded(pages.kata_individual)
+
+    ok_button.click
+    switch_to_editor_window
+    assert_page_loaded(pages.kata_edit)
+  end
 
   def switch_to_editor_window(index = 1)
     @browser.switch_to_window(index)
