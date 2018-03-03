b7643c8381073ba1d0f22fde7e1008565c9cee1f
Jon Jagger <jon@jaggersoft.com>
2018-03-03 17:35:18 +0000

    delete dead code

--- tests/all_tests.rb
+++ tests/all_tests.rb
@@ -5,17 +5,6 @@ def run_tests
   test_filenames.each do |filename|
     require filename
   end
-
-  #require '/tests/test_Homepage.rb'
-  #require '/tests/test_Creating_a_kata.rb'
-  #require '/tests/test_Running_tests_in_a_kata.rb'
-  #require '/tests/test_Diffing_in_a_kata.rb'
-  #require '/tests/test_Join_a_kata.rb'
-  #require '/tests/test_Rejoin_a_kata.rb'
-  #require '/tests/test_Review_a_kata.rb'
-  #require '/tests/test_Renaming_files_in_a_kata.rb'
-  #require '/tests/test_Monitoring_a_kata_with_the_dashboard.rb'
-
 end
 
 if ENV['browser'] == 'chrome'
