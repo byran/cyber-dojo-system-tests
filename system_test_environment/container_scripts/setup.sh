943edf5a1bcbbf66e659672bc82f7267567046dd
Byran Wills-Heath <byran@adgico.co.uk>
2018-03-05 17:26:15 +0000

    Changed to using 'gem install' instead of 'bundler' in the docker image build as I cannot get bundler to work in the docker image

--- system_test_environment/container_scripts/setup.sh
+++ system_test_environment/container_scripts/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
 
-# install packages need to build the gems
-apk --update --no-cache add build-base libffi-dev
-# install bash
-apk --update --no-cache add bash
+# install packages need to build the gems and bash
+apk --update --no-cache add build-base libffi-dev bash
 # make it the default shell for root user
 sed -i -e "s/bin\/ash/bin\/bash/" /etc/passwd
 
-bundler install
+gem install selenium-webdriver -v 3.0.1
+gem install minitest -v 5.8.4
+gem install cucumber -v 2.4.0
+gem install simplecov -v 0.15.0
 
 # remove packages to keep the image small
 apk del build-base libffi-dev
